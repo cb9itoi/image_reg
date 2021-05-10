9ff8926bca2b99decc389b0702f289a6be60f91a
mokostishin <maksim@kostishin.com>
2021-05-10 17:51:26 +0300

    Добавил автоматическое создаиние pdf файла с изображениями сравнения

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from PIL import Image, ImageDraw
+from fpdf import FPDF
+
+
 
 IMAGE_FOLDER = "images/"
 IMAGE_1 = IMAGE_FOLDER + "test3.png"
@@ -7,6 +10,7 @@ IMAGE_2 = IMAGE_FOLDER + "test1.png"
 FACTOR = 1000
 COLS = 100
 ROWS = 100
+
 
 def region_analyze(image, x, y, width, height, factor=FACTOR):
     region_status = 0
@@ -40,10 +44,42 @@ def analyze(image_ref, image_target, col=COLS, row=ROWS):
                 draw = ImageDraw.Draw(reference)
                 draw.rectangle((x - 1, y - 1, x + block_width, y + block_height), outline="red")
 
+
     if has_diff:
         reference.save("diff.png")
+
+
     else:
-        print("Images are identical on: FACTOR {f}, COLS {c}, ROWS {r}".format(f=FACTOR, c=COLS, r=ROWS))
-    reference.save("diff.png")
+       print("Изображения идентичны: FACTOR {f}, COLS {c}, ROWS {r}".format(f=FACTOR, c=COLS, r=ROWS))
+
+
+class PDF(FPDF):
+    def header(self):
+        # Logo
+        self.image('images/test1.png', 5, 8, 100)
+        self.image('diff.png', 110, 8, 100)
+
+# Create a PDF object
+pdf = PDF('P', 'mm', 'Letter')
+
+# get total page numbers
+pdf.alias_nb_pages()
+
+# Set auto page break
+pdf.set_auto_page_break(auto=True, margin=15)
+
+# Add Page
+pdf.add_page()
+
+# specify font
+pdf.add_font('DejaVu', '', 'DejaVuSans-ExtraLight.ttf', uni=True)
+pdf.set_font('DejaVu', '', 14)
+pdf.cell(5, 160, txt="Исходное изображение", ln=0, align="L")
+pdf.cell(180, 160, txt="Полученное изображение", ln=0, align="R")
+
+pdf.output('pdf_report.pdf')
+
+
+
 
 analyze(IMAGE_1, IMAGE_2)
